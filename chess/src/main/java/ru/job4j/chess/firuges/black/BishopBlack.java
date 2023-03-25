1d1c94eb24a81f6883a7e3779a2789700d5afd6d
semyon <semyon.mihalich@gmail.com>
2023-03-25 15:06:25 +0500

    3. Каркас шахматной доски [#792]

--- chess/src/main/java/ru/job4j/chess/firuges/black/BishopBlack.java
+++ chess/src/main/java/ru/job4j/chess/firuges/black/BishopBlack.java
@@ -18,12 +18,55 @@ public class BishopBlack implements Figure {
 
     @Override
     public Cell[] way(Cell dest) {
-        throw new ImpossibleMoveException(
-                String.format("Could not way by diagonal from %s to %s", position, dest)
-        );
+       if (!isDiagonal(position, dest)) {
+            throw new ImpossibleMoveException(
+                    String.format("Could not move by diagonal from %s to %s", position, dest)
+            );
+        }
+        //
+        int xp = position.getX();
+        int yp = position.getY();
+        int x = xp;
+        int y = yp;
+        int xd = dest.getX();
+        int yd = dest.getY();
+        int deltaX = 0;
+        int deltaY = 0;
+        int size = Math.abs(xp - xd);
+        Cell[] steps = new Cell[size];
+        //вверх в лево
+        if(xp > xd && yp < yd) {
+            deltaX = -1;
+            deltaY = 1;
+        }
+        // вниз в лево
+        if(xp > xd && yp > yd) {
+            deltaX = -1;
+            deltaY = -1;
+        }
+        //вверх вправо
+        if(xp < xd && yp > yd) {
+            deltaX = 1;
+            deltaY = -1;
+        }
+        //вниз в право
+        if(xp < xd && yp < yd) {
+            deltaX = 1;
+            deltaY = 1;
+        }
+        for (int index = 0; index < size; index++) {
+        x += deltaX;
+        y += deltaY;
+        steps[index] = Cell.findBy(x, y);
+         }
+        return steps;
     }
 
     public boolean isDiagonal(Cell source, Cell dest) {
+        if (Math.abs(source.getX() - dest.getX()) == Math.abs(source.getY() - dest.getY())) {
+            return true;
+        }
+
         return false;
     }
 
