24bc7c18cdefc6ea67019f6734081d753169f113
semyon <semyon.mihalich@gmail.com>
2023-03-27 12:01:40 +0500

    3. Каркас шахматной доски [#792 #404097]

--- chess/src/test/java/ru/job4j/chess/LogicTest.java
+++ chess/src/test/java/ru/job4j/chess/LogicTest.java
@@ -8,7 +8,6 @@ import ru.job4j.chess.firuges.black.BishopBlack;
 import static org.assertj.core.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
-
 public class LogicTest {
 
     @Test
--- chess/src/test/java/ru/job4j/chess/firuges/black/BishopBlackTest.java
+++ chess/src/test/java/ru/job4j/chess/firuges/black/BishopBlackTest.java
@@ -9,21 +9,21 @@ import static org.assertj.core.api.Assertions.assertThat;
 class BishopBlackTest {
 
     @Test
-    void whenTruePosition() {
+    public void whenTruePosition() {
         Cell expected = Cell.A4;
         Figure bishopBlack = new BishopBlack(expected);
         assertThat(bishopBlack.position()).isEqualTo(expected);
     }
 
     @Test
-    void whenTrueway() {
+    public void whenTrueWay() {
         Cell[] expected = new Cell[]{Cell.D2, Cell.E3, Cell.F4, Cell.G5};
         Figure bishopBlack = new BishopBlack(Cell.C1);
         assertThat(bishopBlack.way(Cell.G5)).isEqualTo(expected);
     }
 
     @Test
-    void whenTrueСopy() {
+    public void whenTrueCopy() {
         Cell expected = Cell.A4;
         Figure bishopBlack = new BishopBlack(expected);
         Cell copy = Cell.B5;
